e92d6faf26ff566ff3bf3d1d140396f1faad430c
Arist0v <arist0v@disroot.org>
2022-05-02 12:03:04 -0400

    work on dropdown

--- js/extension.js
+++ js/extension.js
@@ -103,7 +103,7 @@
 			`
 			this.temperature_property.forEach((property) => {
 				for (let thing in things){
-					if (this.temperature_property,indexOf(things[thing]['id'])){
+					if (this.temperature_property.indexOf(things[thing]['id'])){
 						alert('found thing')
 
 					}
