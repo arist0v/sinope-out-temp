9c395249b5beb6d0d0d6551ddc64af1bb9bab0c1
Arist0v <arist0v@disroot.org>
2022-05-02 13:19:26 -0400

    css for dropdown

--- js/extension.js
+++ js/extension.js
@@ -97,7 +97,7 @@
 		}
 
 		get_dropDown(things){
-			let tempDropdown = `
+			let tempDropdown = `<div id=\'extension-sinope-out-temp-dropdown\'>
 			<select name=\'sensor\' id=\'sensor\'>
 			 <option value=\'none\'>None</option>
 			`
@@ -122,7 +122,7 @@
 				}
 			})
 
-			tempDropdown = tempDropdown + '</select>'
+			tempDropdown = tempDropdown + '</select></div>'
 			return tempDropdown
 		}
 
