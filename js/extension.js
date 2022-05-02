c5bdc17dda289e506bf66419c60f33fa69db6f55
arist0v <arist0v@users.noreply.github.com>
2022-05-01 20:53:38 -0400

    Temp sensor check (#12)
    if no source available, show a message on the screen

--- js/extension.js
+++ js/extension.js
@@ -8,10 +8,6 @@
 			
 			this.sinopeMacOUI = "500b914"
 
-			this.sinope_thermostats = this.get_sinope_thermostat();
-			
-			this.temperature_property = this.get_temp_property();
-            
             this.content = '';
 			fetch(`/extensions/${this.id}/views/content.html`)
 			.then((res) => res.text())
@@ -25,81 +21,85 @@
         }
 
         show(){
-            let testDiv = 'extension-sinope-out-temp-test'
-    		if(this.content == ''){
-    			return;
-    		}
-    		else{
-    			this.view.innerHTML = this.content;
-    		}
-			console.log(this.sinope_thermostats.length);
-			if(this.sinope_thermostats.length < 1){
-				document.getElementById('extension-sinope-out-temp-warning')
-				.innerHTML = 'No sinope Thermostat found. Did you have any on the gateway?'
-				return;
-			}
-			document.getElementById(testDiv)
-			.innerHTML = 'TEST'
-            /*
-			window.API.postJson(
-				`/extensions/${this.id}]/api/init`,
-				{'action':'init' }
-			).then((body) => { 
-				console.log("init response: ");
-				console.log(body);
-                
-				if( body['state'] != true ){
-					console.log("response was OK");
+			API.getThings().then((things)=>{
+				let testDiv = 'extension-sinope-out-temp-test';
+				this.sinope_thermostats = this.get_sinope_thermostat(things);
+				this.temperature_property = this.get_temp_property(things);
+				if(this.content == ''){
+					return;
 				}
-                else{
-                    console.log("response was not OK");
-                }
+				else{
+					this.view.innerHTML = this.content;
+				}
 
-			}).catch((e) => {
-				alert("connection error");
-			});*/
-            
+				console.log(this.sinope_thermostats.length);
+				if(this.sinope_thermostats.length < 1){
+					document.getElementById('extension-sinope-out-temp-warning')
+					.innerHTML = 'No sinope Thermostat found. Did you have any on the gateway?'
+					return;
+				}
+				
+				if(this.temperature_property.length < 1){
+					document.getElementById('extension-sinope-out-temp-warning')
+					.innerHTML = 'No Temperature Property found on other devices.'
+					return;
+				}
+				document.getElementById(testDiv)
+				.innerHTML = 'TEST'
+				/*
+				window.API.postJson(
+					`/extensions/${this.id}]/api/init`,
+					{'action':'init' }
+				).then((body) => { 
+					console.log("init response: ");
+					console.log(body);
+					
+					if( body['state'] != true ){
+						console.log("response was OK");
+					}
+					else{
+						console.log("response was not OK");
+					}
+
+				}).catch((e) => {
+					alert("connection error");
+				});*/
+			})
         }
 
-		get_sinope_thermostat(){
+		get_sinope_thermostat(things){
 			console.log('get sinope thermostats')
-			let sinope_theromstats = []
-			API.getThings().then((things)=>{
+			let sinopeTheromstats = []
 				
-				for (let key in things){
-					if ((things[key]['@type'] == "Thermostat") && (things[key]['id']
-					.indexOf(this.sinopeMacOUI) >= 0)){
-						if (!sinope_theromstats.includes(things[key]['id'])){
-							//console.log('thermostats id: ' + things[key]['id'])
-							sinope_theromstats.push(things[key]['id']);
-						}
+			for (let key in things){
+				if ((things[key]['@type'] == "Thermostat") && (things[key]['id']
+				.indexOf(this.sinopeMacOUI) >= 0)){
+					if (!sinopeTheromstats.includes(things[key]['id'])){
+						//console.log('thermostats id: ' + things[key]['id'])
+						sinopeTheromstats.push(things[key]['id']);
 					}
 				}
-				//console.log('sinope thermostats: '+ sinope_theromstats)
-				
-			})
-			return sinope_theromstats;
-		}
+			}
+			return sinopeTheromstats;
+		};
 
-		get_temp_property(){
+		get_temp_property(things){
 			let tempProperty = []
-			API.getThings().then((things)=> {
-				for (let thing in things){
-					let thingID = things[thing]['id']
-					for (let property in things[thing]['properties']){
-							let propertyTitle = things[thing]['properties'][property]['title']
-							if (things[thing]['properties'][property]['@type'] == "TemperatureProperty"){
-								if (!tempProperty.includes([thingID, propertyTitle])){
-									if (thingID.indexOf(this.sinopeMacOUI) < 0){
-										tempProperty.push([thingID, propertyTitle]);
-									}
+			for (let thing in things){
+				let thingID = things[thing]['id']
+				for (let property in things[thing]['properties']){
+						let propertyTitle = things[thing]['properties'][property]['title']
+						if (things[thing]['properties'][property]['@type'] == "TemperatureProperty"){
+							if (!tempProperty.includes([thingID, propertyTitle])){
+								if (thingID.indexOf(this.sinopeMacOUI) < 0){
+									tempProperty.push([thingID, propertyTitle]);
 								}
 							}
-					}
-				}	
-			})
+						}
+				}
+			}	
 			return tempProperty;
-		}
+		};
     }
     
     new sinopeOutTemp();
