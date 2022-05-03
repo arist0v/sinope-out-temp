404929cea458edc18d361ef3ab3f0fdcf5e610d2
Arist0v <arist0v@disroot.org>
2022-05-02 20:05:41 -0400

    save button action

--- js/extension.js
+++ js/extension.js
@@ -141,9 +141,7 @@
 						let thingName = things[thing]['title']
 						for (let thingProperty in things[thing]['properties']){
 							if (property[1] == things[thing]['properties'][thingProperty]['title']){
-								console.log('HEY: ' + this.sinope_link + ":" + ['sensor_' + property[0], property[1]])
-								console.log(this.sinope_link.indexOf(['sensor_'+ property[0], property[1]]))
-								if (this.sinope_link.indexOf(['sensor_'+ property[0], property[1]]) >= 0){
+								if (this.sinope_link.hasOwnProperty('sensor_'+ property[0])){
 									selected = 'selected'
 								}
 								tempDropdown = tempDropdown +
