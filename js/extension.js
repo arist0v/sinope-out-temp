6fe76e3029956239666470dbb7a097f9dd4150c5
Arist0v <arist0v@disroot.org>
2022-05-03 10:14:47 -0400

    debug

--- js/extension.js
+++ js/extension.js
@@ -141,6 +141,7 @@
 						for (let thingProperty in things[thing]['properties']){
 							if (property[1] == things[thing]['properties'][thingProperty]['title']){
 								if (this.sinope_link.hasOwnProperty('sensor_'+ sinopeID)){
+									console.log(thingName+'_'+ property[1])
 									if (this.sinope_link['sensor_'+ sinopeID].value == thingName 
 									+'_'
 									+ property[1]){
