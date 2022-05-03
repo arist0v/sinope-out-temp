1cc9cf3688894cc3b19c60817390f5c4302be611
Arist0v <arist0v@disroot.org>
2022-05-03 09:52:46 -0400

    debug

--- js/extension.js
+++ js/extension.js
@@ -83,7 +83,7 @@
 		save_config(){
 			let data = {}
 			document.forms['thermostat_form'].forEach((formData) => {				
-				console.log(formData.name)
+				console.log(formData.name + ' : ' + formData.value)
 				//data[formData[0]] = formData[1]
 			})
 			//localStorage.removeItem('sinope_link')
