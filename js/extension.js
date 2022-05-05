b57d43af9dc3a29fb4a3c78ad4ee9ad6ac5d0a44
Arist0v <arist0v@disroot.org>
2022-05-05 19:48:18 -0400

    debug

--- js/extension.js
+++ js/extension.js
@@ -5,7 +5,7 @@
             super('sinope-out-temp');
             this.addMenuEntry('Sinope');
 			this.sinopeMacOUI = "500b914"
-			this.sinope_link = this.load_link()
+
             this.content = '';
 			fetch(`/extensions/${this.id}/views/content.html`)
 			.then((res) => res.text())
@@ -20,8 +20,8 @@
 
         show(){
 			API.getThings().then((things)=>{
-				//this.sinope_link = this.load_link();
-				
+				this.sinope_link = this.load_link();
+				sleep(1000)
 				console.log(typeof this.sinope_link)
 				let warningDiv = 'extension-sinope-out-temp-warning';
 				let listDiv = 'extension-sinope-out-temp-list';
