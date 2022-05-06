a2ac090617882c7361307728f2b8c9a8f92c0ce2
Arist0v <arist0v@disroot.org>
2022-05-05 20:01:46 -0400

    debug

--- js/extension.js
+++ js/extension.js
@@ -20,7 +20,7 @@
 
         show(){
 			API.getThings().then((things)=>{
-				this.load_link();
+				this.sinope_link = this.load_link();
 				
 				console.log(typeof this.sinope_link)
 				let warningDiv = 'extension-sinope-out-temp-warning';
@@ -112,7 +112,7 @@
 					 }
 				 }
 				 console.log(typeof return_value)
-				 Object.assign(this.sinope_link, return_value)
+				 return Object.assign({}, return_value)
 			 }).catch((e)=>{
 
 			 })			
